453b0e0ee860f26aaf3c0f3a9a4aed052e0e6de1
andrey@upo-04 <andrey@upo-04>
2009-02-12 08:41:37 -0700

    blah

---
+++ py9p2/debug.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+def exceptions(type, value, tb) :
+	import pdb
+	import traceback
+	traceback.print_exception(type, value, tb)
+	print
+	pdb.pm()
+
+import sys
+sys.excepthook = exceptions
+
