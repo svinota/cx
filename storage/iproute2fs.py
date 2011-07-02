61c59bc43da5821d9bb743418066c4decf487c79
Peter V. Saveliev <peet@altlinux.org>
2011-07-02 18:45:28 +0400

    * update README * move interfaces to the separate directory

--- storage/iproute2fs.py
+++ storage/iproute2fs.py
@@ -115,13 +115,20 @@ class RootDir(Inode):
         Inode.__init__(self,"/",self,qtype=py9p.DMDIR,storage=storage)
         self.storage = storage
         self.child_map = {
+            "README":       ReadmeInode,
+            "ifmap":        MapInode,
+            "interfaces":   IfacesDir,
+        }
+
+class IfacesDir(Inode):
+    def __init__(self,name,parent):
+        Inode.__init__(self,name,parent,qtype=py9p.DMDIR)
+        self.child_map = {
             "*":        InterfaceDir,
-            "README":   ReadmeInode,
-            "ifmap":    MapInode,
         }
 
     def sync_children(self):
-        return [ x['dev'] for x in iproute2.get_all_links() ] + ['README','ifmap']
+        return [ x['dev'] for x in iproute2.get_all_links() ]
 
 class WrappedIO(object):
     def sync(self):
@@ -158,12 +165,30 @@ class ReadmeInode(Inode):
         Inode.__init__(self,name,parent)
         self.data = StringIO("""
         This filesystem is exported from a python script with 9P protocol.
-        Interface data is obtained via rtnetlink protocol.
+        Interface data is obtained realtime via rtnetlink protocol.
 
         9P protocol: http://9p.cat-v.org/documentation/rfc/
         Source code: http://projects.radlinux.org/cx/browser/cx/storage/iproute2fs.py
 
-        You can get all source tree wirh git clone git://projects.radlinux.org/cx
+        You can get all source tree with git clone git://projects.radlinux.org/cx
+        Please note, that the project is in early alpha.
+
+        ...
+
+        Almost all data is yet read-only, but one can change interface addresses
+        just by editing interfaces/.../addresses file. Something like that:
+
+        Set an address:
+        echo 192.168.0.1/24 >interfaces/eth0/addresses
+
+        Add one more address:
+        echo -e '192.168.0.2/24\\n192.168.0.3/24' >>interfaces/eth0/addresses
+
+        Remove an address:
+        sed -i '/192.168.0.3/d' interfaces/eth0/addresses
+
+        Flush all addresses:
+        cat /dev/null >interfaces/eth0/addresses
 """)
 
 class InterfaceInode(Inode):
