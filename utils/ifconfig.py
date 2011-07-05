f47bbf9b95187f2eaa99c7d00dcf901bba4009b8
Peter V. Saveliev <peet@altlinux.ru>
2011-07-05 18:35:08 +0400

    include ifconfig into speed tests

---
+++ utils/ifconfig.py
@@ -0,0 +1,44 @@
+"""
+An isolated code from http://git.fedorahosted.org/git/?p=vdsm.git;a=blob;f=vdsm/netinfo.py
+
+For speed testing
+"""
+
+# Copyright 2009-2010 Red Hat, Inc. and/or its affiliates.
+#
+# Licensed to you under the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.  See the files README and
+# LICENSE_GPL_v2 which accompany this distribution.
+#
+
+import subprocess
+
+def ifconfig():
+     """ Partial parser to ifconfig output """
+
+     p = subprocess.Popen(["/sbin/ifconfig", '-a'],
+             close_fds=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+             stderr=subprocess.PIPE)
+     out, err = p.communicate()
+     ifaces = {}
+     for ifaceblock in out.split('\n\n'):
+         if not ifaceblock: continue
+         addr = netmask = hwaddr = ''
+         for line in ifaceblock.splitlines():
+             if line[0] != ' ':
+                 ls = line.split()
+                 name = ls[0]
+                 if ls[2] == 'encap:Ethernet' and ls[3] == 'HWaddr':
+                     hwaddr = ls[4]
+             if line.startswith('          inet addr:'):
+                 sp = line.split()
+                 for col in sp:
+                     if ':' not in col: continue
+                     k, v = col.split(':')
+                     if k == 'addr':
+                         addr = v
+                     if k == 'Mask':
+                         netmask = v
+         ifaces[name] = {'addr': addr, 'netmask': netmask, 'hwaddr': hwaddr}
+     return ifaces
--- utils/time.py
+++ utils/time.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from cxnet.netlink.iproute2 import iproute2
+from ifconfig import ifconfig
 from sys import argv
 import timeit
 
@@ -10,8 +11,10 @@ if len(argv) < 2:
 else:
     tc = int(argv[1])
 
-links = timeit.Timer("a = iproute2.get_all_links()","from __main__ import iproute2")
-addrs = timeit.Timer("a = iproute2.get_all_addrs()","from __main__ import iproute2")
+links = timeit.Timer("iproute2.cache = {}; a = iproute2.get_all_links()","from __main__ import iproute2")
+addrs = timeit.Timer("iproute2.cache = {}; a = iproute2.get_all_addrs()","from __main__ import iproute2")
+ifcfg = timeit.Timer("a = ifconfig()","from __main__ import ifconfig")
 
 print "links timeit per %s cycles: %s" % (tc,links.timeit(tc))
 print "addrs timeit per %s cycles: %s" % (tc,addrs.timeit(tc))
+print "ifcfg timeit per %s cycles: %s" % (tc,ifcfg.timeit(tc))
