7e3a137267820401551673075e1465f2088cdd34
Peter V. Saveliev <peet@altlinux.ru>
2011-07-07 16:07:11 +0400

    README as a file

--- iproute2fs/iproute2fs.py
+++ iproute2fs/iproute2fs.py
@@ -139,36 +139,19 @@ class LogInode(Inode):
                 print "add %s" % (item)
                 self.data.write("%s %s\n" % (t,str(item)))
 
-class ReadmeInode(Inode):
+###
+#
+#
+#
+class FileInode(Inode):
     def __init__(self,name,parent):
         Inode.__init__(self,name,parent)
-        self.data = StringIO("""
-        This filesystem is exported from a python script with 9P protocol.
-        Interface data is obtained realtime via rtnetlink protocol.
+        f = open(self.fname,"r")
+        self.data = StringIO(f.read())
 
-        9P protocol: http://9p.cat-v.org/documentation/rfc/
-        Source code: http://projects.radlinux.org/cx/browser/cx/storage/iproute2fs.py
+class ReadmeInode(FileInode):
+    fname = "README"
 
-        You can get all source tree with git clone git://projects.radlinux.org/cx
-        Please note, that the project is in early alpha.
-
-        ...
-
-        Almost all data is yet read-only, but one can change interface addresses
-        just by editing interfaces/.../addresses file. Something like that:
-
-        Set an address:
-        echo 192.168.0.1/24 >interfaces/eth0/addresses
-
-        Add one more address:
-        echo -e '192.168.0.2/24\\n192.168.0.3/24' >>interfaces/eth0/addresses
-
-        Remove an address:
-        sed -i '/192.168.0.3/d' interfaces/eth0/addresses
-
-        Flush all addresses:
-        cat /dev/null >interfaces/eth0/addresses
-""")
 
 class InterfaceInode(Inode):
     def __init__(self,name,parent):
