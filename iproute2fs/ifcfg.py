c35b905a7965d26c3933e7c09b11b38dafde5b55
Peter V. Saveliev <peet@altlinux.ru>
2011-07-19 16:59:11 +0400

    adjust output parser to fit new format

--- iproute2fs/ifcfg.py
+++ iproute2fs/ifcfg.py
@@ -6,4 +6,4 @@ from cxnet.netlink.iproute2 import iproute2
 links = iproute2.get_all_links()
 
 print("%-16s%-16s%-16s%-16s\n" % ("name","type","state","wireless"))
-[ print("%-16s%-16s%-16s%-16s" % (x["dev"],x["link_type"][7:],x["state"],x["wireless"])) for x in links ]
+[ print("%-16s%-16s%-16s%-16s" % (x["dev"],x["link_type"],x["state"],x["wireless"])) for x in links ]
