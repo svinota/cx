b9a32f934fbd4773e9a1f7826c6624c4f287b2a1
Paul Wolneykien <manowar@altlinux.ru>
2011-09-23 01:51:39 +0400

    Define message classes
    Add the generated definition of the message classes.
    See the `templates' branch for more information.

--- lib/cxnet/cxnet/cx9p/messages.py
+++ lib/cxnet/cxnet/cx9p/messages.py
@@ -86,3 +86,393 @@ class p9msgarray (Structure):
         Returns a byte array type of the corresponding length.
         """
         return c_ubyte * self.len
+
+
+# The 9P version implemented
+VERSION9P = "9P2000"
+
+
+class Tcreate (Structure):
+    """
+    9P type 114 'create' request (transmit) message class
+    Open, create - prepare a fid for I/O on an existing or new file
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("fid", c_uint32),
+    ]
+
+    class tail (Structure):
+        """
+        The static tail of the outer message class.
+        """
+        _pack_ = 1
+        _fields_ = [
+            ("perm", c_uint32),
+            ("mode", c_ubyte),
+        ]
+
+    def cdarclass (self, index = 0):
+        """
+        Returns the type of the message tail number ``index``:
+          * name ```p9msgstring```;
+          * ```self.tail```.
+        """
+        if index < 1:
+            return p9msgstring
+        if (index - 1) < 1:
+            return self.tail
+        raise IndexError("Array index out of bounds")
+
+class Rcreate (Structure):
+    """
+    9P type 115 'create' reply (return) message class
+    Open, create - prepare a fid for I/O on an existing or new file
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("qid", p9qid),
+        ("iounit", c_uint32),
+    ]
+
+
+class Tremove (Structure):
+    """
+    9P type 122 'remove' request (transmit) message class
+    Remove - remove a file from a server
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("fid", c_uint32),
+    ]
+
+class Rremove (Structure):
+    """
+    9P type 123 'remove' reply (return) message class
+    Remove - remove a file from a server
+    """
+
+
+class Tversion (Structure):
+    """
+    9P type 100 'version' request (transmit) message class
+    Version - negotiate protocol version
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("msize", c_uint32),
+    ]
+
+    def cdarclass (self):
+        """
+        Returns the type of the message tail ``version``
+        """
+        return p9msgstring
+
+class Rversion (Structure):
+    """
+    9P type 101 'version' reply (return) message class
+    Version - negotiate protocol version
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("msize", c_uint32),
+    ]
+
+    def cdarclass (self):
+        """
+        Returns the type of the message tail ``version``
+        """
+        return p9msgstring
+
+
+class Tstat (Structure):
+    """
+    9P type 124 'stat' request (transmit) message class
+    Stat, wstat - inquire or change file attributes
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("fid", c_uint32),
+    ]
+
+class Rstat (Structure):
+    """
+    9P type 125 'stat' reply (return) message class
+    Stat, wstat - inquire or change file attributes
+    """
+
+    def cdarclass (self):
+        """
+        Returns the type of the message tail ``stat``
+        """
+        return p9msgarray
+
+
+class Tclunk (Structure):
+    """
+    9P type 120 'clunk' request (transmit) message class
+    Clunk - forget about a fid
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("fid", c_uint32),
+    ]
+
+class Rclunk (Structure):
+    """
+    9P type 121 'clunk' reply (return) message class
+    Clunk - forget about a fid
+    """
+
+
+class Tauth (Structure):
+    """
+    9P type 102 'auth' request (transmit) message class
+    Attach, auth - messages to establish a connection
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("afid", c_uint32),
+    ]
+
+    def cdarclass (self, index = 0):
+        """
+        Returns the type of the message tail number ``index``:
+          * uname ```p9msgstring```;
+          * aname ```p9msgstring```.
+        """
+        if index < 1:
+            return p9msgstring
+        if (index - 1) < 1:
+            return p9msgstring
+        raise IndexError("Array index out of bounds")
+
+class Rauth (Structure):
+    """
+    9P type 103 'auth' reply (return) message class
+    Attach, auth - messages to establish a connection
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("aqid", p9qid),
+    ]
+
+
+class Topen (Structure):
+    """
+    9P type 112 'open' request (transmit) message class
+    Open, create - prepare a fid for I/O on an existing or new file
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("fid", c_uint32),
+        ("mode", c_ubyte),
+    ]
+
+class Ropen (Structure):
+    """
+    9P type 113 'open' reply (return) message class
+    Open, create - prepare a fid for I/O on an existing or new file
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("qid", p9qid),
+        ("iounit", c_uint32),
+    ]
+
+
+class Tflush (Structure):
+    """
+    9P type 108 'flush' request (transmit) message class
+    Flush - abort a message
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("oldtag", c_uint16),
+    ]
+
+class Rflush (Structure):
+    """
+    9P type 109 'flush' reply (return) message class
+    Flush - abort a message
+    """
+
+
+class Tread (Structure):
+    """
+    9P type 116 'read' request (transmit) message class
+    Read, write - transfer data from and to a file
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("fid", c_uint32),
+        ("offset", c_uint64),
+        ("count", c_uint32),
+    ]
+
+class Rread (Structure):
+    """
+    9P type 117 'read' reply (return) message class
+    Read, write - transfer data from and to a file
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("count", c_uint32),
+        ("data", ),
+    ]
+
+
+class Terror (Structure):
+    """
+    9P type 106 'error' request (transmit) message class
+    Error - return an error
+    Comment: illegal 
+    """
+
+class Rerror (Structure):
+    """
+    9P type 107 'error' reply (return) message class
+    Error - return an error
+    """
+
+    def cdarclass (self):
+        """
+        Returns the type of the message tail ``ename``
+        """
+        return p9msgstring
+
+
+class Twalk (Structure):
+    """
+    9P type 110 'walk' request (transmit) message class
+    Walk - descend a directory hierarchy
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("fid", c_uint32),
+        ("newfid", c_uint32),
+        ("nwname", c_uint16),
+    ]
+
+    def cdarclass (self):
+        """
+        Returns the type of the message tail ``wname``
+        """
+        return p9msgstring
+
+class Rwalk (Structure):
+    """
+    9P type 111 'walk' reply (return) message class
+    Walk - descend a directory hierarchy
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("nwqid", c_uint16),
+        ("qid", (p9qid * nwqid)),
+    ]
+
+
+class Tattach (Structure):
+    """
+    9P type 104 'attach' request (transmit) message class
+    Attach, auth - messages to establish a connection
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("fid", c_uint32),
+        ("afid", c_uint32),
+    ]
+
+    def cdarclass (self, index = 0):
+        """
+        Returns the type of the message tail number ``index``:
+          * uname ```p9msgstring```;
+          * aname ```p9msgstring```.
+        """
+        if index < 1:
+            return p9msgstring
+        if (index - 1) < 1:
+            return p9msgstring
+        raise IndexError("Array index out of bounds")
+
+class Rattach (Structure):
+    """
+    9P type 105 'attach' reply (return) message class
+    Attach, auth - messages to establish a connection
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("qid", p9qid),
+    ]
+
+
+class Twstat (Structure):
+    """
+    9P type 126 'wstat' request (transmit) message class
+    Stat, wstat - inquire or change file attributes
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("fid", c_uint32),
+    ]
+
+    def cdarclass (self):
+        """
+        Returns the type of the message tail ``stat``
+        """
+        return p9msgarray
+
+class Rwstat (Structure):
+    """
+    9P type 127 'wstat' reply (return) message class
+    Stat, wstat - inquire or change file attributes
+    """
+
+
+class Twrite (Structure):
+    """
+    9P type 118 'write' request (transmit) message class
+    Read, write - transfer data from and to a file
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("fid", c_uint32),
+        ("offset", c_uint64),
+        ("count", c_uint32),
+        ("data", ),
+    ]
+
+class Rwrite (Structure):
+    """
+    9P type 119 'write' reply (return) message class
+    Read, write - transfer data from and to a file
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("count", c_uint32),
+    ]
+
+# The tuple of all defined message classes
+p9msgclasses = tuple()
+p9msgclasses += tuple([None]*100) # Types for 0..99 are not defined
+p9msgclasses += tuple([Tversion, Rversion]) # 100, 101
+p9msgclasses += tuple([Tauth, Rauth]) # 102, 103
+p9msgclasses += tuple([Tattach, Rattach]) # 104, 105
+p9msgclasses += tuple([Terror, Rerror]) # 106, 107
+p9msgclasses += tuple([Tflush, Rflush]) # 108, 109
+p9msgclasses += tuple([Twalk, Rwalk]) # 110, 111
+p9msgclasses += tuple([Topen, Ropen]) # 112, 113
+p9msgclasses += tuple([Tcreate, Rcreate]) # 114, 115
+p9msgclasses += tuple([Tread, Rread]) # 116, 117
+p9msgclasses += tuple([Twrite, Rwrite]) # 118, 119
+p9msgclasses += tuple([Tclunk, Rclunk]) # 120, 121
+p9msgclasses += tuple([Tremove, Rremove]) # 122, 123
+p9msgclasses += tuple([Tstat, Rstat]) # 124, 125
+p9msgclasses += tuple([Twstat, Rwstat]) # 126, 127
+p9msgclasses += tuple([None]*128) # Types for 128..255 are not defined
+
+# Export some constants
+__all__ += ["VERSION9P"]
+# Export all defined message types
+__all__ += export_by_prefix("T",globals()) + export_by_prefix("R",globals())
